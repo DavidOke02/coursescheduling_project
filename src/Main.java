385ce9131cc51b88829f4c87779ab4a2b8bb37de
DavidOke02 <doo5136@psu.edu>
2025-04-13 22:03:19 -0400

    Finalized implementation of displayCourse in CourseListViewController

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         //App start (From login screen)
-        //LoginController loginController = new LoginController();
-        CourseListViewController controller2= new CourseListViewController();
+        LoginController loginController = new LoginController();
 
         //Db Connect
         Connection connection = DBConnection.getConnection();
