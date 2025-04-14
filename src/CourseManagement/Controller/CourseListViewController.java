0f279317c57d278805375941db6d74226bb740bb
DavidOke02 <doo5136@psu.edu>
2025-04-13 20:38:50 -0400

    Initial implementation of CourseListViewController

--- src/CourseManagement/Controller/CourseListViewController.java
+++ src/CourseManagement/Controller/CourseListViewController.java
@@ -1,4 +1,30 @@
 package CourseManagement.Controller;
 
+import CourseManagement.Model.Course;
+import CourseManagement.View.CourseListView;
+import db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CourseListViewController {
+    CourseListView courseListView;
+
+    public CourseListViewController() {
+        System.out.println("Displaying Course List View...");
+        courseListView = new CourseListView(this);
+    }
+
+    public void displayCourseList(){
+        Connection connection = DBConnection.getConnection();
+
+        try{
+            PreparedStatement displayCourseList = connection.prepareStatement("SELECT * FROM Course");
+            ResultSet resultSet = displayCourseList.executeQuery();
+    } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Could not display course list.");
+        }
 }
