25ba3e1cebdfb4b485d3e076b9a76e3f47c44466
chirasvigowda <czg5634@psu.edu>
2025-04-27 19:26:36 -0400

    initial implementation of CourseDetailSearchController.java

--- src/Scheduling/Controller/CourseDetailSearchController.java
+++ src/Scheduling/Controller/CourseDetailSearchController.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class CourseDetailSearchController {
     private CourseDetailView detailView;
     private CourseSearchView searchView;
