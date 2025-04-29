eb1bba2108fccb2451fdc983e306e8e5e64d3f15
chirasvigowda <czg5634@psu.edu>
2025-04-28 20:01:18 -0400

    initial design of RegisterCourseView

--- src/Scheduling/View/RegisterCourseView.java
+++ src/Scheduling/View/RegisterCourseView.java
@@ -6,6 +6,19 @@ import javax.swing.*;
 
 public class RegisterCourseView extends JFrame {
     private JPanel basePanel;
+    private JLabel Heading;
+    private JLabel Course_Name;
+    private JLabel Course_ID;
+    private JTextField CourseNameField;
+    private JTextField CourseIDField;
+    private JTextField InstructorField;
+    private JLabel Instructor;
+    private JLabel Credits;
+    private JTextField CreditsField;
+    private JLabel Department_Code;
+    private JTextField DepartmentCodeField;
+    private JButton registerCourseButton;
+
     /**
      * Allows a student to register for a course.
      */
